ddb3a85eba19747f1992e5a4197f28d3d585d747
ToldoDM <bertoldo.damiano@gmail.com>
2021-04-07 17:58:22 +0200

    aggiornate settings

--- bc19api/settings.py
+++ bc19api/settings.py
@@ -82,22 +82,19 @@ WSGI_APPLICATION = 'bc19api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if sys.argv[1] == 'test':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': str(BASE_DIR / 'my.cnf'),
+        },
+    },
+    'unittest': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'OPTIONS': {
-                'read_default_file': '~/Desktop/UniPD/SWE/bc19-api/my.cnf',
-            },
-        }
-    }
+}
 
 
 # Password validation
