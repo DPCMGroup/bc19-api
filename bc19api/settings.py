4c6a2fb85ce3cf4406b9f4e9ee9d01ee0826c09f
ToldoDM <bertoldo.damiano@gmail.com>
2021-05-16 17:13:48 +0200

    aggiunto allowed host all

--- bc19api/settings.py
+++ bc19api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '*_s3km*fc-f*lr%scx=&@o&o+grv91!cqp^t+8e%@t_o&i*wl5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
