b1ad8c5a54db24e7f65c5c5e164a1a6fc67f579b
ToldoDM <bertoldo.damiano@gmail.com>
2021-06-02 11:42:12 +0200

    sistemato presentazione codice

--- AdminApp/Views/report_view.py
+++ AdminApp/Views/report_view.py
@@ -3,14 +3,16 @@ from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
 from AdminApp.cron import createOccupationReport, createSanificationReport
 
+
 @require_http_methods(["POST"])
 def getOccupationReport(request):
     data = JSONParser().parse(request)
-    return JsonResponse(createOccupationReport(data['starttime'].strftime("%Y-%m-%d %H:%M"), data['endtime'].strftime("%Y-%m-%d %H:%M")))
+    return JsonResponse(createOccupationReport(data['starttime'].strftime("%Y-%m-%d %H:%M"),
+                                               data['endtime'].strftime("%Y-%m-%d %H:%M")))
 
 
 @require_http_methods(["POST"])
 def getSanitizationReport(request):
     data = JSONParser().parse(request)
-    return JsonResponse(createSanificationReport(data['starttime'].strftime("%Y-%m-%d %H:%M"), data['endtime'].strftime("%Y-%m-%d %H:%M")))
-
+    return JsonResponse(createSanificationReport(data['starttime'].strftime("%Y-%m-%d %H:%M"),
+                                                 data['endtime'].strftime("%Y-%m-%d %H:%M")))
